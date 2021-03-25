fbf4563c659d4407206a813dc9d0ca7daa301d34
Michele Lisignoli <michele@lisignoli.com>
2021-03-25 17:17:33 +0100

    prev image - next image

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue(
+   {
+      el: "#app",
+      data:{
+         images: [
+            "https://www.canonclubitalia.com/comunicati-stampa/uploads/2016/10/Andrea-Shooter-Resta%E2%80%8E.jpg",
+            "https://www.calendariovaltellinese.com/images/blog/chiavenna-andrea-resta.jpg",
+            "https://www.calendariovaltellinese.com/images/eventi/chiavenna-a-resta.jpg",
+            "https://www.calendariovaltellinese.com/images/eventi/chiavenna%20andrea%20shooter%20resta.jpg",
+            "https://www.celesteprize.com/indi/_pics/8/3/2015-53851-27554_18316_4565_t.jpg?m=1433408930",
+            "https://i.ytimg.com/vi/ysTSmeb7VRM/maxresdefault.jpg"
+         ],
+         imageIndex: 0,
+      },
+      methods:{
+         nextImage: function(){
+            this.imageIndex++;
+            if (this.imageIndex == this.images.length) {
+               this.imageIndex = 0
+            }
+         },
+         prevImage: function(){
+            this.imageIndex--;
+            if (this.imageIndex < 0) {
+               this.imageIndex = this.images.length - 1
+            }
+         }
+      }
+   }
+);
